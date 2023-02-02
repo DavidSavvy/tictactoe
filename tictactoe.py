f3953cce9c3ae1164cf12299866561112af85d8d
dave <davesavenok@gmail.com>
2023-02-02 08:18:04 -0600

    Post AB prune, final version

--- tictactoe.py
+++ tictactoe.py
@@ -64,7 +64,6 @@ def result(board, action):
 def winner(board):
     """
     Returns the winner of the game, if there is one.
-    - Might be problematic with the way the winner is decided using the current player(board), keep in mind
     """
     current_player = None
     if player(board) == X:
@@ -72,10 +71,12 @@ def winner(board):
     else:
         current_player = X
     
+    # Checks rows and columns for winner
     for i in range(0,3):
         if board[i][0] == board[i][1] == board[i][2] != EMPTY or board[0][i] == board[1][i] == board[2][i] != EMPTY:
             return current_player
     
+    # Checks diagonals for winner
     if board[0][0] == board[1][1] == board[2][2] != EMPTY or board[0][2] == board[1][1] == board[2][0] != EMPTY:
         return current_player
     
@@ -115,43 +116,63 @@ def minimax(board):
     
     def min_function(working_board):
         min_value = 1
+        # Ends recursion to go up the call stack
         if terminal(working_board):
             return utility(working_board)
         else:
+            # Recurses back to opposite function for every possible move
             for action in actions(working_board):
                 min_value = min(min_value, max_function(result(working_board, action)))
+                # Short alpha-beta prune, to stop when reached lowest value, saves recursion time
+                if min_value == -1:
+                    return min_value
             return min_value
+
 
     def max_function(working_board):
         max_value = -1
+        # Ends recursion to go up the call stack
         if terminal(working_board):
             return utility(working_board)
         else:
+            # Recurses back to opposite function for every possible move
             for action in actions(working_board):
                 max_value = max(max_value, min_function(result(working_board, action)))
+                # Short alpha-beta prune, to stop when reached highest value, saves recursion time
+                if max_value == 1:
+                    return max_value
             return max_value
 
+    # Runs minimax algorithm for max player
     if player(board) == X:
         final_actions = []
+        # Runs for every current action
         for action in actions(board):
+            # Starts recursion to find value of the current action
             current_value = min_function(result(board, action))
+            
+            # Expedites recursion a little
             if current_value == 1:
                 return action
             final_actions.append((current_value, action))
-        print (final_actions)
+
         final_index = final_actions.index(max(final_actions))
         return final_actions[final_index][1]
 
+    # Runs minimax algorithm for min player
     elif player(board) == O:
         final_actions = []
+        # Runs for every current action
         for action in actions(board):
+            # Starts recursion to find value of the current action
             current_value = max_function(result(board, action))
+            
+            # Expedites recursion a little
             if current_value == -1:
                 return action
             final_actions.append((current_value, action))
-        print (final_actions)
+        
         final_index = final_actions.index(min(final_actions))
-        print(final_index)
         return final_actions[final_index][1]
     
 
